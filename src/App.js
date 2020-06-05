22ebcb141a2d32725a0afe326e68b7565bb48392
AppVeyor <ci@appveyor.com>
2020-06-05 17:38:30 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TrainingHistory from './components/TrainingHistory';
+
+function App() {
+  return (
+    <div className="container">
+        <TrainingHistory />
+    </div>
+  );
+}
+
+export default App;
